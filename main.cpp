26ab4a8b1397ff1239a58595a49e237ef7087ac1
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-07-09 12:50:12 +0200

    Add header comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+// Program name: MultiUserAddresseeList (vectors applied)
+// Status: Code of feature/user-input branch
+// Date: 09.07.2025
+// Author: Jaroslaw Dolinski
+
 #include <iostream>
 
 using namespace std;
