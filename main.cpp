9cd4664c2085d6b57fefc0f014605c0c7085ee32
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-07-13 21:05:05 +0200

    Refactor parameter types of saveDataAfterRemovingOrEditingAddressee

--- main.cpp
+++ main.cpp
@@ -373,7 +373,7 @@ void showAllAddressees(const vector <Addressee>& addressees) {
     waitForKeyPress();
 }
 
-void saveDataAfterRemovingOrEditingAddressee(int idOfLoggedUser, int removedAddresseeId, string addresseesFilename, const string& editedLine = "" ) {
+void saveDataAfterRemovingOrEditingAddressee(const int idOfLoggedUser, const int removedAddresseeId, const string& addresseesFilename, const string& editedLine = "" ) {
     fstream addresseesFile, temporaryFile;
     string line, id, userId, firstName, lastName, phone, email, address, dummy;
 
