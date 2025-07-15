f88ec502c69cd690483dbc25496677b53095dfbe
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-07-15 10:46:22 +0200

    Correct comment of signUp

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ void appendNewUserToUsersFile(const User& user, const string& filename) {
     }
 }
 
-// Register a new user and return number of logged users
+// Register a new user
 void signUp(vector<User>& users, const string& filename) {
     string username, password;
     int numberOfUsers = users.size();
